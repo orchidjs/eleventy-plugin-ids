192820f765b68d714f0f37f078fec8b04be1eb59
Josh Schmidt <oyejorge@gmail.com>
2021-07-06 22:00:07 -0600

    add code/readme/package.json

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # eleventy-plugin-ids
+
 @11ty plugin for adding ids to html headings and other elements
+
+```html
+<h1>Foo Bar</h1>
+```
+will become
+
+```html
+<h1 id="foo-bar">Foo Bar</h1>
+```
+
+## Installation
+
+```
+npm install https://github.com/orchidjs/eleventy-plugin-ids
+```
+
+## Basic Usage
+
+Add eleventy-plugin-ids to your .eleventy.js file
+```js
+module.exports = function(eleventyConfig) {
+	//...
+	
+	const anchors_plugin = require('@orchidjs/eleventy-plugin-ids');
+	eleventyConfig.addPlugin(anchors_plugin);
+	
+	//...
+}
+```
+
+
+## Settings
+
+```js
+module.exports = function(eleventyConfig) {
+	//...
+	
+	const anchors_plugin = require('@orchidjs/eleventy-plugin-ids');
+	eleventyConfig.addPlugin(anchors_plugin,{
+		selectors: ['h1','h2','h3','h4','h5','h6'],
+		prefix: 'custom-id-prefix-',
+		formatter: function(element,existing_ids_array){
+			return '--generate-a-custom-id-here-',
+		}
+	});
+	
+	//...
+}
+```
---
+++ eleventy-ids.js
@@ -0,0 +1,41 @@
+
+module.exports = function( eleventyConfig, config = {} ) {
+
+	const { JSDOM }	= require('jsdom');
+	const selectors	= config.selectors || ['h1','h2','h3','h4','h5','h6'];
+	const prefix	= config.prefix || '';
+	const formatter	= config.formatter || function(element,existing_ids){
+		
+		var id = config.prefix + eleventyConfig.javascriptFunctions.slug(element.textContent).replace(/[&,+()$~%.'":*?!<>{}]/g, "");
+		
+		if( existing_ids.indexOf(id) >= 0 ){}
+		
+		return id;
+	};
+
+	
+	// add meta tag to each html page
+	eleventyConfig.addTransform("ids", async (rawContent, outputPath) => {
+	
+		if( !outputPath || !outputPath.endsWith(".html") ){
+			return rawContent;
+		}
+		
+		const dom = new JSDOM(rawContent);
+		const ids = [];
+		selectors.forEach((selector)=>{
+			dom.window.document.querySelectorAll(selector).forEach( (element) => {
+				
+				var id = element.getAttribute('id')
+				if( id != undefined ){
+					ids.push(id);
+					return;
+				}
+				
+				element.setAttribute('id',formatter(element,ids));
+			});
+		});
+
+		return dom.serialize();
+	});
+}
